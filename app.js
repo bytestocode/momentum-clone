9791e1c9d167a0a84fac1960c346c8dca8057b60
reload1bronze <nature9th@gmail.com>
2021-10-22 10:44:58 +0900

    control-css-in-js

--- app.js
+++ app.js
@@ -1,41 +1,13 @@
 const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  h1.style.color = "blue";
-  console.log("h1 was clicked!");
-}
-
-function handleMouseEnter() {
-  h1.innerText = "mouse is here!";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "mouse is out!";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("offline!");
-}
-
-function handleWindowOnline() {
-  alert("All good!");
+  // const clickedClass = "clicked"
+  // if (h1.classList.contains(clickedClass)) {
+  //   h1.classList.remove(clickedClass);
+  // } else {
+  //   h1.classList.add(clickedClass);
+  // }
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
-// h1.onclick =  handleTitleClick;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
-
