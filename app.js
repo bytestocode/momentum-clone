9a99f7a900e42c82a0b267d6c3b63d05908a68ca
reload1bronze <nature9th@gmail.com>
2021-10-22 14:36:17 +0900

    event-prevent

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const h1 = document.querySelector(".hello h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function handleTitleClick() {
-  // const clickedClass = "clicked"
-  // if (h1.classList.contains(clickedClass)) {
-  //   h1.classList.remove(clickedClass);
-  // } else {
-  //   h1.classList.add(clickedClass);
-  // }
-  h1.classList.toggle("clicked");
+const HIDDEN_CLASSNAME = "hidden";
+
+// const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-h1.addEventListener("click", handleTitleClick);
+// function handleLinkClick(event) {
+//   event.preventDefault();
+//   alert("click!");
+// }
+
+loginForm.addEventListener("submit", onLoginSubmit);
+// link.addEventListener("click", handleLinkClick);
